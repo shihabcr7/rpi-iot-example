ee5bf1384d165bb9e3d6211379e7a2b4d18f1aba
shihabcr7 <shihabtallinn@gmail.com>
2017-01-17 19:35:49 +0200

    python code added to blink the LED

---
+++ blink.py
@@ -0,0 +1,36 @@
+
+led1 = 6  # GPIO number where the led is connected
+led2 = 13
+led3 = 26
+source = 19
+# Tell the GPIO module to use GPIO numbering used by processor
+GPIO.setmode(GPIO.BCM)
+
+
+# Set GPIO no 10 to output mode
+GPIO.setup(led1, GPIO.OUT)
+GPIO.setup(led2, GPIO.OUT)
+GPIO.setup(led3, GPIO.OUT)
+GPIO.setup(source, GPIO.OUT)
+
+GPIO.output(led1, True)
+GPIO.output(led2, True)
+GPIO.output(led3, True)
+GPIO.output(source, True)
+
+
+
+# Blink some leds
+try:
+    while True:
+        GPIO.output(led1, False)
+        sleep(1)  # Sleep for 1 second
+        GPIO.output(led1, True)
+        GPIO.output(led2, False)
+        sleep(1)
+        GPIO.output(led2, True)
+        GPIO.output(led3, False)
+        sleep(1)
+        GPIO.output(led3, True)
+except:
+    GPIO.cleanup()
